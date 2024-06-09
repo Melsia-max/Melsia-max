e790ecd8c3c658b64126cedb237d9875412228dc
Melsia-max <mels.sam4@outlook.com>
2024-06-09 23:51:52 +0530

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,14 @@
+#single line comment
+#addition of two numbers
+a = 3
+b = 5
+c = a + b 
+print(c)
+print('=========================')
+
+#Double line comment
+
+"""
+Add two numbers a and b
+print the sum 
+"""
